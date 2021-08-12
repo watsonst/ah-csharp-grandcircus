6b495df007803d653078925e697d06553ee825f5
Paul Riviera <paulr@nuolab.com>
2021-08-12 19:24:54 -0400

    add recursive example from 2021.08.12

---
+++ 20210812/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _20210812
+{
+    class Program
+    {
+        public static void myCopyMethod(char[] strOne, char[] strTwo, int index)
+        {
+            if (index < 0)
+            {
+                return;
+            }
+
+            strTwo[index] = strOne[index];
+
+            myCopyMethod(strOne, strTwo, index - 1);
+        }
+
+        public static void myCopyMethodWithLoop(char[] strOne, char[] strTwo)
+        {
+            for (var i = 0; i < strOne.Length; i++)
+            {
+                strTwo[i] = strOne[i];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string testString = "ThePowerIsOut";
+
+            char[] testStringChar = testString.ToCharArray();
+            char[] newStringChar = new char[testString.Length];
+
+            Console.WriteLine(testStringChar.Length);
+            Console.WriteLine(newStringChar.Length);
+
+            //myCopyMethod(testStringChar, newStringChar, newStringChar.Length - 1);
+            myCopyMethodWithLoop(testStringChar, newStringChar);
+
+            Console.WriteLine(testStringChar);
+            Console.WriteLine(newStringChar);
+        }
+
+    }
+}
